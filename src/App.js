cb463ad338af51ee64fdd41c8f9fd6f8fad89a3c
Monica Hill <monica.r.hill@gmail.com>
2021-09-28 10:52:19 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component}  from 'react';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from "./pages/home";
+import Portfolio from "./pages/portfolio";
+import Contact from "./pages/contact";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+
+    return (
+      <Router>
+          <Switch>
+              <Route  path='/contact'><Contact /></Route>
+              <Route  path='/portfolio'><Portfolio /></Route>
+              <Route exact path='/'><Home /></Route>
+           </Switch>
+  </Router>
+
+    )
 }
 
-export default App;
---
+++ src/Utils/API.js
@@ -0,0 +1,5 @@
+// import React from "react";
+// import Home from "./pages/home";
+// import Portfolio from "./pages/portfolio";
+// import Contact from "./pages/contact";
+
